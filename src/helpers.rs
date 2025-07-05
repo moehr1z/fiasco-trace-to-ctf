9292ca67bd95d25b739e26e32997e6f111243305
moehr1z <mooehr1z@gmail.com>
2025-07-05 15:11:07 +0200

    Discard strings which contain invalid ascii in name conversion

--- src/helpers.rs
+++ src/helpers.rs
@@ -2,10 +2,14 @@ use core::str;
 use std::str::Utf8Error;
 
 pub fn i8_array_to_string<const N: usize>(array: [i8; N]) -> Result<String, Utf8Error> {
-    // NOTE i am not sure why, but the name field in the Nam event sometimes has some gibberish bytes
-    // after the first null byte, so we can't just use from_utf8
     let end = array.iter().position(|&c| c == 0).unwrap_or(array.len());
 
     let bytes: Vec<u8> = array[..end].iter().map(|&c| c as u8).collect();
-    str::from_utf8(&bytes).map(|s| s.to_string())
+
+    if bytes.iter().any(|&b| !(b.is_ascii_graphic() || b == b' ')) {
+        #[allow(invalid_from_utf8)]
+        return Err(str::from_utf8(&[0xFF]).unwrap_err()); // fake UTF-8 error 
+    }
+
+    String::from_utf8(bytes).map_err(|e| e.utf8_error())
 }
